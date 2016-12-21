556ee0759f97952877b5fc42bfa5ef86d7907956
Adolfo Castro <adolfo@desarrollo.local>
2016-12-21 17:42:08 -0500

    update app upload play store

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,1 +1,12 @@
 Meteor.subscribe("userData");
+
+jQuery(document).ready(function($){
+	var altoPantalla = $(window).height();
+	
+	setTimeout(function() {
+		$(".container").css({"min-height" : altoPantalla+"px"});
+		$(".loginIn").css({"top" : altoPantalla/10 +"px"});
+	}, 80);
+	
+
+})
