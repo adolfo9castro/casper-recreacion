79aa74558774a4fb2f475f1a50a5b461febe0ba7
Adolfo Castro <adolfo@desarrollo.local>
2017-01-10 18:41:16 -0500

    Add android version 4

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,22 +1,31 @@
 Meteor.subscribe("userData");
-if (Meteor.isClient) {
-	WebFontConfig = {
-      google: { families: ['Rancho', 'PT Sans' ] }
-    };    
 
-    jQuery(document).ready(function($){
-		var altoPantalla = $(window).height();
-		
-		setTimeout(function() {
-			$(".container").css({"min-height" : altoPantalla+"px"});
-			var wf = document.createElement('script');
-		      wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-		        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-		      wf.type = 'text/javascript';
-		      wf.async = 'true';
-		      var s = document.getElementsByTagName('script')[0];
-		      s.parentNode.insertBefore(wf, s);
-		      console.log("async fonts loaded", WebFontConfig);
-		}, 1000);
-	});
-}
+WebFontConfig = {
+  google: { families: ['Rancho', 'PT Sans' ] }
+};    
+
+jQuery(document).ready(function($){
+	var altoPantalla = $(window).height();
+	
+	setTimeout(function() {
+		$(".container").css({"min-height" : altoPantalla+"px"});
+		var wf = document.createElement('script');
+	      wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	      wf.type = 'text/javascript';
+	      wf.async = 'true';
+	      var s = document.getElementsByTagName('script')[0];
+	      s.parentNode.insertBefore(wf, s);
+
+	    $("#openMenuPerfil").click(function(){
+			$(".menuOpen").css({"left":"0"});
+		});
+
+		$("#closeMenuPerfil").click(function(){
+			$(".menuOpen").css({"left":"-100%"});
+		});
+
+	}, 1600);
+
+	
+});
