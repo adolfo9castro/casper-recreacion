3ef848542abb072eedd7c59e9662e5844080f1bf
Adolfo Castro <desarrollo@smarttree.co>
2017-02-03 18:30:53 -0500

    add content

--- client/helpers/body.js
+++ client/helpers/body.js
@@ -5,10 +5,36 @@ Template.layout.onRendered(function() {
 
   setTimeout(function(){
     $(".loaderContent").fadeOut("slow");
+    $("#map").css({"min-height" : altoPantalla+"px"});
     setTimeout(function(){
       $(".loaderContent").remove();
     }, 900);
   }, 1000);
 
+  $("#geolocalizacion").click(function(){
+    $(".mapas").fadeIn();
+    $(".recreacionistas").fadeOut("fast");
+    $(".noticias").fadeOut("fast");
+    $(".amiguitos").fadeOut("fast");
+  });
+  $("#recreacionistas").click(function(){
+    $(".recreacionistas").fadeIn();
+    $(".mapas").fadeOut("fast");
+    $(".noticias").fadeOut("fast");
+    $(".amiguitos").fadeOut("fast");
+  });
+  $("#amiguitos").click(function(){
+    $(".amiguitos").fadeIn();
+    $(".recreacionistas").fadeOut("fast");
+    $(".mapas").fadeOut("fast");
+    $(".noticias").fadeOut("fast");
+
+  })
+  $("#noticias").click(function(){
+    $(".noticias").fadeIn();
+    $(".recreacionistas").fadeOut("fast");
+    $(".mapas").fadeOut("fast");
+    $(".amiguitos").fadeOut("fast");
+  })
 
 });
