1ee68aced1bd72f18889bddad1385313977139f6
Adolfo Castro <desarrollo@smarttree.co>
2017-03-27 17:34:10 -0500

    add button friend

--- client/templates/includes/menuInvited.js
+++ client/templates/includes/menuInvited.js
@@ -1,9 +1,16 @@
-Template.menuInvited.onRendered(function() {
-    $("#facebook_friends").on("click",function(){
-    	console.log("Hola")
+Template.menuInvited.onRendered(function() {    
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.7&appId=270775685307";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+    /*$("#facebook_friends").on("click",function(){
 	    FB.ui({
 	        method: 'apprequests',
 	        message: 'Invitación a mi aplicación de facebook unodepiera'
 	    });
-    })
+    })*/
 });
