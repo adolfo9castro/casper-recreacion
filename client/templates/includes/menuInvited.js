a5ce9c841d10bc259c67de92f441c65013dc2fb2
Adolfo Castro <desarrollo@smarttree.co>
2017-03-27 18:36:36 -0500

    add button friend

--- client/templates/includes/menuInvited.js
+++ client/templates/includes/menuInvited.js
@@ -1,15 +1,6 @@
 Template.menuInvited.onRendered(function() {
 	
-	setTimeout(function(){
-		alert("carga menuInvited");
-		(function(d, s, id) {
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.7&appId=1318056228245794";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	}, 8000);
+	
     /*$("#facebook_friends").on("click",function(){
 	    FB.ui({
 	        method: 'apprequests',
