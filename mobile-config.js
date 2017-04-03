d1098028cb2e591166fdda93ea7637b8fd9cd91d
Adolfo Castro <desarrollo@smarttree.co>
2017-04-03 15:52:11 -0500

    add on github

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,7 @@
 App.accessRule('http://*');
 App.accessRule('https://*');
 App.accessRule('*');
+App.accessRule('https://*.graph.facebook.com/*');
 App.accessRule('https://*.googleapis.com/*');
 App.accessRule('https://*.google.com/*');
 App.accessRule('https://*.gstatic.com/*');
